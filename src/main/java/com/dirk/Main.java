98593396dcdcb990330fa7e5c473f945677a4bae
Dirk Harrington <dirk@stackframe.com>
2014-01-05 22:46:41 -0500

    Initial Commit

---
+++ src/main/java/com/dirk/Main.java
@@ -0,0 +1,31 @@
+package com.dirk;
+
+import java.util.Map.Entry;
+import java.util.Properties;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ *
+ * @author dirk
+ */
+public class Main extends ResourceConfig {
+
+    public Main() {
+        packages("com.dirk");
+        
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind("woot").named("FUBAR");
+            }
+            
+            private void _bindProperties(Properties props) {
+                for (Entry<Object, Object> p : props.entrySet()) {
+                    bind(p.getValue()).named((String) p.getKey());
+                }
+            }
+        }); 
+    }
+    
+}
---
+++ src/main/java/com/dirk/Resource.java
@@ -0,0 +1,37 @@
+package com.dirk;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource/<anything>" path)
+ */
+@Path("myresource/{p}")
+public class Resource {
+
+    // A simple example of custom injection
+    @Inject
+    @Named("FUBAR")
+    private String fubar;
+    
+    // A simple example of field injection
+    @PathParam("p")
+    private String p;
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String plain() {
+        return String.format("Got it - hot redeploy - BOOM! %s and %s", fubar, p);
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String json() {
+        return String.format("{\"%s\":\"%s\"}", fubar, p);
+    }
+}
